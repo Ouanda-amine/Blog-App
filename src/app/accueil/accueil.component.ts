1f9ebced39a0b957fc55ba33a76b4054a6fc30b6
amiine <amineouanda@gmail.com>
2025-04-24 13:01:32 +0100

    creation du service , composant et db.json

---
+++ src/app/accueil/accueil.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {PostServiceService} from '../post-service.service';
+import {Router} from '@angular/router';
+import {NgForOf} from '@angular/common';
+
+interface Post {
+  id: number;
+  titre: string; // Au lieu de 'titre'
+  auteur: string; // Au lieu de 'auteur'
+}
+
+@Component({
+  selector: 'app-accueil',
+  imports: [
+    NgForOf
+  ],
+  templateUrl: './accueil.component.html',
+  styleUrl: './accueil.component.css'
+})
+export class AccueilComponent  implements  OnInit{
+
+  allposts : Post[] = []
+
+  constructor( private  service : PostServiceService , private router : Router) {}
+
+  ngOnInit(): void {
+    this.service.getAll().subscribe(data=>
+      this.allposts = data
+    )
+  }
+
+
+}
